3ff8d03e50ff8e7a1c8c78aee4d03b3ad94dce77
zwx <m957915339@163.com>
2019-03-04 14:03:36 +0800

    add photos

--- gitee.py
+++ gitee.py
@@ -126,10 +126,11 @@ def handle_photo():
         date_str, info = filename.split("_")
         info, _ = info.split(".")
         date = datetime.strptime(date_str, "%Y-%m-%d")
-        year_month = date_str[0:7]            
+        year_month = date_str[0:9]            
         if i == 0:  # 处理第一个文件
-            new_dict = {"date": year_month, "arr":{'year': date.year,
+            new_dict = {"date": year_month_day, "arr":{'year': date.year,
                                                                    'month': date.month,
+																   'day': date.day,
                                                                    'link': [filename],
                                                                    'text': [info],
                                                                    'type': ['image']
