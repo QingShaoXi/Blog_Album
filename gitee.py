cef36c5fac1283f4460943b7cd393a0eb5be9552
zwx <m957915339@163.com>
2019-03-04 13:39:33 +0800

    add photos

--- gitee.py
+++ gitee.py
@@ -151,7 +151,7 @@ def handle_photo():
             list_info[-1]['arr']['type'].append('image')
     list_info.reverse()  # 翻转
     final_dict = {"list": list_info}
-    with open("F:/Yilia_blog/source/instagram/data.json","w") as fp:
+    with open("F:/hexo博客系统/Yilia_blog/source/instagram/data.json","w") as fp:
         json.dump(final_dict, fp)
 
 def cut_photo():
